fe4f854d5229cdc3c7d199317123dd12f5301670
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-12-07 22:21:03 +0000

    fixed button problems

--- src/components/elements/button/Button.tsx
+++ src/components/elements/button/Button.tsx
@@ -3,13 +3,29 @@ import styles from "./Button.module.scss";
 import classnames from "classnames";
 
 type Props = {
-    children: any
+    children: any,
+    style?: 'google' | 'facebook',
+    func?: () => void,
 }
 
 function Button(props: Props) {
-    const { children } = props;
+    const { children, style, func } = props;
+
+    const handleClick = () => {
+        if(func) {
+            return func();
+        }else {
+            return null;
+        }
+    };
+
     return (
-        <button className={classnames(styles.root)}>
+        <button onClick={handleClick} className={classnames(
+            styles.root,
+            { [styles.icon]: style },
+            { [styles.google]: style === 'google' },
+            { [styles.facebook]: style === 'facebook' },
+        )}>
             {children}
         </button>
     );
