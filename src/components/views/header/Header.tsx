b3ae2fa560d135e348bf4de29c466254fb279dfd
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-12-07 23:20:40 +0000

    Added header to page

---
+++ src/components/views/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classnames from 'classnames';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <div className={classnames(styles.root)}>
+      
+    </div>
+  );
+};
+
+export default Header;
