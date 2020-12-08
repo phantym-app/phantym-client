d0b373e29372948bdb9af5d7b5639b3e7b2a99fb
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-12-08 21:41:19 +0000

    pulled profile menu out of header

--- src/components/views/header/Header.tsx
+++ src/components/views/header/Header.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classnames from 'classnames';
 import styles from './Header.module.scss';
 import friends from '../../../assets/icons/users.svg';
 import notifications from '../../../assets/icons/bell.svg';
 import user from '../../../assets/icons/user.svg';
-import visible from '../../../assets/icons/eye.svg';
-import settings from '../../../assets/icons/cog.svg';
-import logOut from '../../../assets/icons/log-out.svg';
-import { useHeader } from './HeaderState';
+import ProfileMenu from './menus/ProfileMenu/ProfileMenu';
 
 const Header = () => {
-  const { activeMenu, setActiveMenu } = useHeader();
+  const [activeMenu, setActiveMenu] = useState('');
+
   return (
     <header className={classnames(styles.root)}>
       <div className={classnames(styles.logo)} />
@@ -30,28 +28,7 @@ const Header = () => {
               activeMenu === 'profile' ? '' : 'profile'
             )}>
             <img src={user} alt={'user'} /></button>
-          <div className={classnames(
-            styles.profileMenu,
-            { [styles.active]: activeMenu === 'profile' }
-          )}>
-            <div className={classnames(styles.triangle)}/>
-            <button className={classnames(styles.option)}>
-              <img src={user} alt={'profile'} />
-              <p>Profile</p>
-            </button>
-            <button className={classnames(styles.option)}>
-              <img src={visible} alt={'visible'} />
-              <p>Visible</p>
-            </button>
-            <button className={classnames(styles.option)}>
-              <img src={settings} alt={'settings'} />
-              <p>Settings</p>
-            </button>
-            <button className={classnames(styles.option)}>
-              <img src={logOut} alt={'profile'} />
-              <p>Log out</p>
-            </button>
-          </div>
+          <ProfileMenu isActive={activeMenu === 'profile'} setMenuState={setActiveMenu} />
         </div>
       </div>
     </header >
