31e1dd6e41dd71bdb808de70aa642318ca694bc7
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-12-09 00:42:54 +0000

    added visible button logic

--- src/components/views/header/menus/ProfileMenu/ProfileMenu.tsx
+++ src/components/views/header/menus/ProfileMenu/ProfileMenu.tsx
@@ -1,11 +1,13 @@
-import React, { useRef, RefObject } from 'react';
+import React, { useRef, RefObject, useState } from 'react';
 import classnames from 'classnames';
 import styles from './ProfileMenu.module.scss';
 import user from '../../../../../assets/icons/user.svg';
 import visible from '../../../../../assets/icons/eye.svg';
+import notVisible from '../../../../../assets/icons/eye-close.svg';
 import settings from '../../../../../assets/icons/cog.svg';
 import logOut from '../../../../../assets/icons/log-out.svg';
 import { useOnClickOutside } from '../../../../../logic/useOnClickOutside';
+import Toggle from '../../../../elements/toggle/Toggle';
 
 type Props = {
   isActive: boolean,
@@ -14,6 +16,7 @@ type Props = {
 
 const ProfileMenu = (props: Props) => {
   const { isActive, setMenuState } = props;
+  const [visibility, setVisibility] = useState(false);
   const ref = useRef<RefObject<HTMLDivElement>>(null);
   useOnClickOutside(ref, () => setMenuState(''));
 
@@ -30,9 +33,10 @@ const ProfileMenu = (props: Props) => {
         <img src={user} alt={'profile'} />
         <p>Profile</p>
       </button>
-      <button className={classnames(styles.option)}>
-        <img src={visible} alt={'visible'} />
+      <button onClick={() => {setVisibility(!visibility); console.log(visibility)}} className={classnames(styles.option, styles.visible)}>
+        <img src={visibility ? visible : notVisible} alt={'visible'} />
         <p>Visible</p>
+        <Toggle checked={visibility} />
       </button>
       <button className={classnames(styles.option)}>
         <img src={settings} alt={'settings'} />
