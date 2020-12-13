041075290a497a3a2e5d1eba3e9e8ff240323559
OliwerDrywa <oliwerdrywa@gmail.com>
2020-12-13 15:06:22 +0000

    preact ssr

--- functions/src/index.tsx
+++ functions/src/index.tsx
@@ -1,20 +1,43 @@
 import * as functions from 'firebase-functions';
 import { h } from 'preact';
-/** @jsx h */
+
 import express from 'express';
 import render from 'preact-render-to-string';
 
-// const Foo = () => (
-//   <div>
-//     <h5>hello</h5>
-//     <p>This page is ssr.</p>
-//   </div>
-// );
+import Game from '../../src/routes/Game';
+
+const template = (innerHtml?: string) => `
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Unity WebGL Player | webFPS</title>
+    <link rel="shortcut icon" href="TemplateData/favicon.ico" />
+    <link rel="stylesheet" href="TemplateData/style.css" />
+
+    <script src="TemplateData/UnityProgress.js"></script>
+    <script src="Build/UnityLoader.js"></script>
+    <script>
+      UnityLoader.instantiate("unityContainer", "Build/Desktop.json", {
+        onProgress: UnityProgress,
+      })
+    </script>
+  </head>
+  <body style="background-color: #202020">
+    <div class="webgl-content" style="width: 90%; height: 90%">
+      <div id="unityContainer" style="width: 100%; height: 100%"></div>
+
+      <div class="fullscreen" onclick="unityInstance.SetFullscreen(1)"></div>
+    </div>
+    ${innerHtml}
+  </body>
+</html>
+`;
 
 const ssr = express().get('*', (req, res) => {
-  // let html = render(h(Foo, {}));
-  // res.send(`<!DOCTYPE html><html><body>${html}</body></html>`);
-  res.send(`<!DOCTYPE html><html><body><h1>hi</h1></body></html>`);
+  // let html = render(<Game />);
+  res.send(template());
 });
 
 exports.ssr = functions.region('europe-west2').https.onRequest(ssr);
--- src/routes/Game/index.tsx
+++ src/routes/Game/index.tsx
@@ -1,13 +1,11 @@
 import { h } from 'preact';
-import type firebase from 'firebase';
+// import type firebase from 'firebase';
 
-const Game = ({ user }: { user: firebase.User }) => {
-  const params = new URLSearchParams(window.location.search);
+const Game = () => {
+  // const params = new URLSearchParams(window.location.search);
 
-  const id = params.get('id') as string;
-  const room = params.get('room');
-
-  // console.log(ref);
+  // const id = params.get('id') as string;
+  // const room = params.get('room');
 
   return <h1>hi</h1>;
 };
