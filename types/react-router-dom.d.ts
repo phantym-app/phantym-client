56cacd31a747202a031fc9ddb228323cb28940f3
OliwerDrywa <oliwerdrywa@gmail.com>
2021-01-18 17:51:46 +0000

    preact type fix to react-router-dom

--- types/react-router-dom.d.ts
+++ types/react-router-dom.d.ts
@@ -1,9 +1,7 @@
 import 'react-router-dom';
 
-declare module 'react-router-dom' {}
-
-// export function Link<S = H.LocationState>(
-//     ...params: Parameters<Link<S>>
-// ): ReturnType<Link<S>>;
-// (alias) function Link<S = unknown>(props: LinkProps<S> & React.RefAttributes<HTMLAnchorElement>): ReturnType<Link<S>>
-// import Link
+declare module 'react-router-dom' {
+  export interface LinkProps {
+    class: any;
+  }
+}
