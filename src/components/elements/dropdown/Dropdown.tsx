47bd011dac75a6f9f37a4bed326ff98d8f421256
Kevin <kvanhelden@nn4m.com>
2021-01-30 21:22:06 +0000

    small changes to dropdown

--- src/components/elements/dropdown/Dropdown.tsx
+++ src/components/elements/dropdown/Dropdown.tsx
@@ -7,18 +7,24 @@ import chevronDown from '@assets/icons/chevron-down.svg';
 interface Props {
   items: string[];
   coloured?: boolean;
+  bigText?: boolean;
+  alignLeft?: boolean;
 }
 
-function Dropdown({ items, coloured }) {
+function Dropdown({ items, coloured, bigText, alignLeft }) {
   const [selectedItem, setSelectedItem] = useState<string>(items[0]);
   const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
 
   return (
     <>
       <div class={styles.root} onClick={() => setMenuIsOpen(!menuIsOpen)}>
-        <p class={{ [styles.coloured]: coloured }}>{selectedItem}</p>
+        {bigText ? (
+          <h6 class={{ [styles.coloured]: coloured }}>{selectedItem}</h6>
+        ) : (
+          <p class={{ [styles.coloured]: coloured }}>{selectedItem}</p>
+        )}
         <img class={{ [styles.menuOpen]: menuIsOpen }} src={chevronDown} alt={'chevron-down'} />
-        <div class={[styles.menu, { [styles.active]: menuIsOpen }]}>
+        <div class={[styles.menu, { [styles.active]: menuIsOpen, [styles.alignLeft]: alignLeft }]}>
           {items.map((item: string) => {
             return (
               <div
