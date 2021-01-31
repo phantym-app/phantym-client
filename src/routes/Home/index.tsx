a6603dc60e3e2af31568e1a20ff26f7630d56cd0
Kevin <kvanhelden@nn4m.com>
2021-01-31 23:34:25 +0000

    finished tablet view

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,8 +1,9 @@
 import { h, Fragment } from 'preact';
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import styles from './Home.module.scss';
 
 import filter from '@assets/icons/filter.svg';
+import search from '@assets/icons/search.svg';
 import mockData from './mockData.json';
 
 import Button from '@components/elements/button/Button';
@@ -11,18 +12,42 @@ import Dropdown from '@components/elements/dropdown/Dropdown';
 import LabelOverview from '@components/collections/labelOverview/LabelOverview';
 import GameOverview from '@components/collections/gameOverview/GameOverview';
 
+import { matchesWidth } from '@logic/matchesWidth';
+
 function Index() {
   const [activeTab, setActiveTab] = useState<string>('allGames');
   const [activeLabels, setActiveLabel] = useState<string[]>([]);
+  const [searchButton, setSearchbutton] = useState<boolean>(matchesWidth('850', 'max'));
   const [searchQuery, setSearchQuery] = useState<string>('');
+
+  useEffect(() => {
+    const searchResize = () => {
+      if (matchesWidth('850', 'max')) {
+        setSearchbutton(true);
+      } else {
+        setSearchbutton(false);
+      }
+    };
+
+    window.addEventListener('resize', searchResize);
+    return () => {
+      window.removeEventListener('resize', searchResize);
+    };
+  }, []);
+
   return (
     <div class={styles.root}>
       <div class={styles.actions}>
         <div class={styles.title}>
           <h1>My games</h1>
           <div class={styles.search}>
-            {/* TODO: Add Search function */}
-            <Searchbar onChange={e => setSearchQuery(e)} placeholder={'Search for a game'} />
+            {searchButton ? (
+              <Button squared colour={'secondary'}>
+                <img src={search} alt={'search'} />
+              </Button>
+            ) : (
+              <Searchbar onChange={e => setSearchQuery(e)} placeholder={'Search for a game'} />
+            )}
             {/* TODO: Add filter functions */}
             <Button squared colour={'secondary'}>
               <img src={filter} alt={'filter'} />
