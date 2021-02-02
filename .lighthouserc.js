1bcfcca49998626236ddc65a798398ad02779b45
OliwerDrywa <oliwerdrywa@gmail.com>
2021-02-02 12:31:18 +0000

    feat: lighthouse ci

---
+++ .lighthouserc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ci: {
+    assert: {
+      preset: 'lighthouse:no-pwa', // lighthouse:recommended
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': 'error',
+
+        // prone to false-positives
+        'unused-javascript': ['warn', { minScore: 0.6, maxLength: 10 }],
+        'unminified-javascript': 'warn',
+        'unused-css-rules': 'warn',
+        'unminified-css': 'warn',
+
+        // not needed
+        'no-unload-listeners': 'off',
+        'unsized-images': 'off',
+
+        // just stupid
+        'valid-source-maps': 'off',
+        'redirects-http': 'off',
+        'is-on-https': 'off',
+      },
+    },
+  },
+};
