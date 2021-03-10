3d0fb2cd2c13796c68fa13bcf34ef2b34cad941d
OliwerDrywa <oliwerdrywa@gmail.com>
2021-03-10 09:45:58 +0000

    feat: minimal useRoom

--- src/store/room.ts
+++ src/store/room.ts
@@ -11,42 +11,28 @@ const firebase$database = import('@logic/firebase/database');
 import type firebase from 'firebase';
 type Reference = firebase.database.Reference;
 type DataSnapshot = firebase.database.DataSnapshot;
-type RoomData = { [uid: string]: { displayName: string } };
+type RoomData = {
+  [uid: string]: {
+    displayName: string;
+    index: number;
+  };
+};
 
-function alertBeforeUnload(bool = true) {
+function alertBeforeUnload(bool: boolean) {
   window.onbeforeunload = bool ? () => 'Are you sure you want to leave this room?' : () => {};
 }
 
 function useRoom() {
   const { user$ } = useAuth();
-  const [roomRef, setRoomRef] = useState<null | Reference>(null);
-  const [playerRef, setPlayerRef] = useState<null | Reference>(null);
   const [roomData, setRoomData] = useState<RoomData>({});
+  const [leaveRoom, setLeaveRoom] = useState<() => any>(null);
 
   const onRoomUpdate = useCallback((snap: DataSnapshot) => setRoomData(snap.val()), []);
 
-  async function __joinRoomForcefully(__roomRef: Reference, index: number) {
-    let { uid, displayName } = await user$;
-    displayName = displayName ?? randomGhost();
-
-    const __playerRef = __roomRef.child(uid);
-
-    // adds player to db
-    __playerRef.onDisconnect().remove();
-    await __playerRef.set({ displayName, index });
-    setPlayerRef(__playerRef);
-
-    // subscribes to db
-    __roomRef.orderByChild('index').on('value', onRoomUpdate);
-    setRoomRef(__roomRef);
-
-    alertBeforeUnload();
-  }
-
-  async function tryJoinRoom(roomId: string) {
+  async function joinRoom(roomId: string) {
     const { db } = await firebase$database;
-    const __roomRef = db.ref(`room/${roomId}`);
-    const room = await new Promise<DataSnapshot>(res => __roomRef.once('value', res));
+    const roomRef = db.ref(`room/${roomId}`);
+    const room = await new Promise<DataSnapshot>(res => roomRef.once('value', res));
 
     // throw if room does not exist
     if (!room.exists()) throw new Error(`room ${roomId} does not exist`);
@@ -57,40 +43,55 @@ function useRoom() {
     // throw if player is already in the room
     if (roomData[uid]) throw new Error(`player already in room ${roomId}`);
 
-    return await __joinRoomForcefully(__roomRef, Object.values(roomData).length);
+    return await __joinRoomForcefully(roomRef, Object.values(roomData).length);
   }
 
   async function createRoom() {
     const roomId = randomRoomId();
 
     const { db } = await firebase$database;
-    const __roomRef = db.ref(`room/${roomId}`);
-    const room = await new Promise<DataSnapshot>(res => __roomRef.once('value', res));
+    const roomRef = db.ref(`room/${roomId}`);
+    const room = await new Promise<DataSnapshot>(res => roomRef.once('value', res));
 
     // check if room already exists, if exists restart function
     if (room.exists()) return await createRoom();
-    else await __joinRoomForcefully(__roomRef, 0);
+    else await __joinRoomForcefully(roomRef);
 
     return roomId;
   }
 
-  async function leaveRoom() {
-    // unsubscribes from the db
-    roomRef.orderByChild('index').off('value', onRoomUpdate);
-    setRoomRef(null);
+  async function __joinRoomForcefully(roomRef: Reference, index = 0) {
+    let { uid, displayName } = await user$;
+    displayName = displayName ?? randomGhost();
 
-    // removes player data in db
-    await playerRef.remove();
-    setPlayerRef(null);
+    const playerRef = roomRef.child(uid);
 
-    alertBeforeUnload(false);
+    playerRef.onDisconnect().remove();
+    await playerRef.set({ displayName, index }); // adds player to db
+
+    roomRef.orderByChild('index').on('value', onRoomUpdate); // subscribes to db
+
+    alertBeforeUnload(true);
+
+    // creates a leaveRoom for clean up
+    setLeaveRoom(
+      () =>
+        function leaveRoom() {
+          roomRef.orderByChild('index').off('value', onRoomUpdate); // unsubscribes from the db
+          playerRef.remove(); // removes player data in db
+
+          alertBeforeUnload(false);
+
+          setLeaveRoom(null);
+        },
+    );
   }
 
   return {
-    inRoom: !!roomRef,
+    inRoom: !!leaveRoom,
     roomData,
 
-    tryJoinRoom,
+    joinRoom,
     createRoom,
     leaveRoom,
   };
