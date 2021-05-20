8cb91f5f7ceeb1b1ddc8c8cdca376a3a5cc6e6c1
OliwerDrywa <oliwerdrywa@gmail.com>
2021-05-20 20:35:49 +0100

    refactor(StateProvider): cleaner api & types

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,3 +1,4 @@
+import type { VNode } from 'preact';
 import { h } from 'preact';
 
 import { AuthProvider } from './auth';
@@ -6,7 +7,16 @@ import { CastProvider } from './cast';
 import { GameLibraryProvider } from './gameLibrary';
 import { DeviceWidthProvider } from './deviceWidth';
 
-const makeCombinedProvider = (...containers) => ({ children }) =>
-  containers.reduce((A, Provider) => <Provider>{A}</Provider>, children);
+const Providers = [
+  // higher can access to lower
+  DeviceWidthProvider,
+  GameLibraryProvider,
+  CastProvider,
+  RoomProvider,
+  AuthProvider,
+];
 
-export default makeCombinedProvider(DeviceWidthProvider, GameLibraryProvider, CastProvider, RoomProvider, AuthProvider);
+const CombinedProvider = ({ children }: { children: VNode<any> }) =>
+  Providers.reduce((A, Provider) => <Provider>{A}</Provider>, children);
+
+export default CombinedProvider;
